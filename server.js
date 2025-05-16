f59f4ef0a9ef1902f6398867c73d15ab49b82fa6
moodeeest <uwed.akmal@gmail.com>
2025-05-17 01:09:42 +0500

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/generate', upload.single('image'), async (req, res) => {
         <style>
           .title { fill: white; font-size: 100px; font-family: Times New Roman; }
           .subtitle { fill: white; font-size: 40px; font-family: Montserrat; }
-          .price { fill: white; font-size: 150px; font-family: Garamond; }
+          .price { fill: white; font-size: 150px; font-family: Times New Roman; }
         </style>
         <text x="50%" y="320" text-anchor="middle" class="title">${district}</text>
         <text x="50%" y="380" text-anchor="middle" class="subtitle">${location}</text>
